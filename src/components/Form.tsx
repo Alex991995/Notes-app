8b1f56437002ffb5a5cda7375922bc5d0898e617
Alex991995 <51911663+Alex991995@users.noreply.github.com>
2023-12-07 22:25:37 +0300

    fix: ref error

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,21 +8,24 @@ function Form() {
   const [value, setValue] = useState('');
   const [hashtag, setHashtag] = useState<RegExpMatchArray | null>();
   const textareraRef = useRef<HTMLTextAreaElement>(null)
+  const ref = useRef<HTMLDivElement>(null)
   const { addNote } = useNote( ({addNote}) => ({addNote}) );
 
   function handelTextarea(e:React.ChangeEvent<HTMLTextAreaElement>) {
     console.log(hashtag)
     setHashtag(value.match(/(#[a-z0-9-_]+)/g) )
     setValue(e.target.value.replace(/(#[a-z0-9-_]+)/g, '<span class="text-red-600">$1</span>'))
-   
   }
 
   function handelAddNote(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
-    if(textareraRef.current !== null && textareraRef.current.value ) {
+    if(textareraRef.current !== null && textareraRef.current.value && ref.current ) {
       const valueFromTextarera = textareraRef.current.value
       const newNote = {id: nanoid(), text:valueFromTextarera }
       addNote(newNote)
+
+      console.log(ref.current?.textContent)
+      ref.current.textContent = ""
       setValue('')
     }
     else return false 
@@ -35,7 +38,7 @@ function Form() {
           placeholder='Type here...' 
           className='text-transparent bg-transparent caret-black w-[30rem] h-[20rem] border border-slate-700 outline-none' ref={textareraRef}
           onChange={handelTextarea}></textarea>
-          <div className='absolute top-0 left-0 w-[30rem] h-[20rem] -z-10 '  dangerouslySetInnerHTML={{ __html: value }}></div>
+          <div className='absolute top-0 left-0 w-[30rem] h-[20rem] -z-10 ' ref={ref}  dangerouslySetInnerHTML={{ __html: value }}></div>
         </div>
         <button className='rounded-md p-3 bg-green-500 hover:bg-green-600 transition-colors'>Button</button>
       </form>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,27 +1,23 @@
 import { create } from "zustand";
 import { persist} from "zustand/middleware";
 
-type NoteType = {
-  notes: Notes[],
-  addNote: (newNote:{ id: string, text:string} ) => void
-}
-
 type Notes = {
   id: string,
   text:string
 }
 
-
+type NoteType = {
+  notes: Notes[],
+  addNote: (newNote:{ id: string, text:string} ) => void
+}
 
 export const useNote = create<NoteType>()(
   persist((set, get) => ({
 
     notes: [],
-    
     addNote: (newNote) => {
-
       set( {notes: [ ...get().notes , newNote] })
-    }
+    },
 
     }),
     {name: "any"}
