07d32ac3242f970587d5de67c8f1981cfaf3bdc3
Alex991995 <sashe.1995@gmail.com>
2024-01-19 20:03:14 +0300

    add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
+import Logo from '../images/writing.png'
+
 function Header() {
   return (
-    <header className="mt-4 ml-4">
-      <h2 className="text-slate-950 text-3xl font-bold">Notes</h2>
+    <header className="mt-4 ml-4 ">
+      <div className='inline-block text-center'>
+        <img src={Logo} alt="logo" className='w-14 inline'/>
+        <h2 className="text-slate-950 text-3xl font-bold">Notes App</h2>
+      </div>
     </header>
   );
 }
